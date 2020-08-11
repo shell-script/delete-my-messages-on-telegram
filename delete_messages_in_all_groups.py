332e4154675c8aed5cb7b269cc4c8cc3a4dedc27
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-08-12 02:27:37 +0800

    delete_messages_in_all_groups: add new method

---
+++ delete_messages_in_all_groups.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#-*-coding:utf-8-*-
+
+from telethon.sync import TelegramClient
+client = TelegramClient('telethon', api_id='', api_hash='')
+client.start()
+
+if input('Are you sure you want to delete your *every outgoing message* in *every group*? (y/n): ') != 'y':
+	exit(0)
+
+message_ids = []
+counter = 0
+
+for dialog in client.get_dialogs():
+	if dialog.is_group:
+		entity = dialog.entity
+		for message in client.iter_messages(entity, from_user=client.get_me()):
+			if len(message_ids) < 300:
+				counter +=1
+				message_ids.append(message.id)
+				continue
+			else:
+				client.delete_messages(client.get_entity(entity), message_ids)
+				message_ids=[]
+		client.delete_messages(client.get_entity(entity), message_ids)
+
+print('%d messages were deleted.' % counter)
+print('Done! (Please run the script two to three times to clean every message. If you run once only, some messages may won\'t be deleted.)')
+print('Some service messages(user joined group and so on) can only be deleted by admins, so the counter will stay the same and it\'s normal.')
