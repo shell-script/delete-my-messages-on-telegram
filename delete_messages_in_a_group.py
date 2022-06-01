213ca82c7983490b2fe6ce052c5a9ed6a12c6dcc
Tianling Shen <cnsztl@immortalwrt.org>
2022-06-02 01:46:24 +0800

    delete_messages_in_a_group: fix api changes
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- delete_messages_in_a_group.py
+++ delete_messages_in_a_group.py
@@ -9,7 +9,7 @@ def command_handler(client, message):
 	if (message.text == "delmymsg") and (message.from_user.id == app.get_me().id):
 		message_ids = []
 		for i in app.search_messages(chat_id=message.chat.id, from_user=message.from_user.id):
-			message_ids.append(i.message_id)
+			message_ids.append(i.id)
 			if len(message_ids) > 99:
 				app.delete_messages(message.chat.id, message_ids)
 				message_ids = []
