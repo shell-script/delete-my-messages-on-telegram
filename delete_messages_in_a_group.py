943df78fa5cc6c424c797012cd721c4b800ed5bf
Tianling Shen <cnsztl@immortalwrt.org>
2022-07-10 20:21:05 +0800

    delete_messages_in_a_group: improve handle channel messages
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- delete_messages_in_a_group.py
+++ delete_messages_in_a_group.py
@@ -4,15 +4,23 @@
 from pyrogram import Client
 app = Client('pyrogram', api_id='', api_hash='')
 
+import logging
+logging.basicConfig(level=logging.WARN)
+
 @app.on_message(filters.me & filters.regex('delmymsg'))
-def command_handler(client, message):
+async def delmymsg_handler(client, message):
 	if message.text == "delmymsg":
-		message.delete()
+		await message.delete()
+		# Require admin privileges
+		# await app.delete_user_history(message.chat.id,
+		# 	(message.from_user and message.from_user.id or message.sender_chat.id))
+
 		message_ids = []
-		for i in app.search_messages(chat_id=message.chat.id, from_user=message.from_user.id):
+		# async for i in app.search_messages(chat_id=message.chat.id, from_user=user_id):
+		async for i in app.search_messages(chat_id=message.chat.id, from_user='me'):
 			message_ids.append(i.id)
 			if len(message_ids) > 99:
-				app.delete_messages(message.chat.id, message_ids)
+				await app.delete_messages(message.chat.id, message_ids)
 				message_ids = []
-		app.delete_messages(message.chat.id, message_ids)
+		await app.delete_messages(message.chat.id, message_ids)
 app.run()
